9893a6e30f94de7d914b4a96a3e19c134d611035
Callum Anderson <cm20cja@leeds.ac.uk>
2025-02-27 16:16:00 +0000

    Fixed rendering the correct amount of tiles. this was due to incorrect application of nexted map.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,11 +4,11 @@ import Tile from "./Tile";
 
 function Board({gameState, handleTileClick}){
     return(
-        <div className="bg-gray-700 grid grid-cols-7 gap-2 w-full aspect-square rounded-lg shadow-lg p-4 pt-4 pb-4">
-            {gameState.map((value,colIndex,column) => (
+        <div className="bg-gray-700 grid grid-cols-7 gap-2 w-full rounded-lg shadow-lg p-4 pt-4 pb-4">
+            {gameState.map((column,colIndex) => (
                 <div className="bg-gray-700 h-full ">
                     {column.map((value,rowIndex) => (
-                        <Tile onClick={handleTileClick} x={colIndex} y={rowIndex} state={gameState[rowIndex][colIndex]}>
+                        <Tile onClick={handleTileClick} x={colIndex} y={rowIndex} state={gameState[colIndex][rowIndex]}>
                         </Tile>))
                     }
                 </div>))
--- src/components/connect4.jsx
+++ src/components/connect4.jsx
@@ -7,10 +7,14 @@ function Connect4(){
     const [gameState,setGameState] = useState(new Array(7).fill(new Array(6).fill("")));
 
     const handleTileClick = (coordinate) =>{
-        console.log(gameState[coordinate.x][coordinate.y])
-        const newState = [...gameState];
-        newState[coordinate.y][coordinate.x] = "HELLO";
-        setGameState(newState);
+        console.log(gameState,coordinate,gameState[coordinate.x][coordinate.y])
+        setGameState((prevState) => {
+            const newState = [...gameState];
+            newState[coordinate.x] = [...newState[coordinate.x]]
+            newState[coordinate.x][coordinate.y] = "hello"
+            return newState;}
+        );
+        
     }
 
     return(
